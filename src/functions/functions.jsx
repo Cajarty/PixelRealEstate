5338425c1abe4957a8c85a42ce32cfdf0ff879a4
Carson Roscoe <CarsonRoscoe7@gmail.com>
2018-01-08 15:08:21 -0800

    Fixed and tested the conversion functions

--- src/functions/functions.jsx
+++ src/functions/functions.jsx
@@ -10,37 +10,31 @@ export const ContractDataToRGBAArray = (/*uint256[10]*/ contractDataArray) => {
     let contractDataArraySize = 10;
     let pixelsPerBigInt = 10;
      
-    for(let i = 0; i < contractDataArraySize; i++) {
+    for(let i = contractDataArraySize - 1; i >= 0; i--) {
         let uint256 = contractDataArray[i]; //Big ass number
-        for(let j = 0; j < pixelsPerBigInt; j++) {
-            //data moved to the right enough to push all but the last rgb values off it
-            let maximumByte = bigInt("FF", 32);
-            let bitwiseOffset = (24 * (pixelsPerBigInt - j + 1));
-            let mask = maximumByte >> bitwiseOffset;
-
-            let binary = uint256 >> bitwiseOffset;
-
-            let rgbData = BinaryToRGB(mask & binary);
-            result.push(rgbData.r);
-            result.push(rgbData.g);
-            result.push(rgbData.b);
-            result.push(0);
+        for (let j = 0; j < pixelsPerBigInt; j++) {
+            result.unshift(0);
+            result.unshift(uint256.and(255).toJSNumber());
+            uint256 = uint256.shiftRight(8); 
+            result.unshift(uint256.and(255).toJSNumber());
+            uint256 = uint256.shiftRight(8);
+            result.unshift(uint256.and(255).toJSNumber());
+            uint256 = uint256.shiftRight(8);
         }
     }
-
     return result;
 }
 
 //Assumption: rgbArray is an array of rgb values for 100 pixels
 export const RGBArrayToContractData = (rgbArray) => {
     let result = [];
-    for(let i = 0; i < 10; i++) {
-        let innerResult = 0;
-        for(let j = 0; j < 10; j++) {
-            let binary = RGBToBinary(rgbArray[i * 30 + j * 3], rgbArray[i * 30 + j * 3 + 1], rgbArray[i * 30 + j * 3 + 2]);
-            if (j != 0)
-                innerResult = innerResult << 24;
-            innerResult = innerResult | binary;
+    let counter = 0;
+    for(let i = 0; i < 10; i++) { //Foreach uint256 in uint256[10]
+        let innerResult = bigInt("0", 10);
+        for(let j = 0; j < 10; j++) { //Foreach 24 bits for the uint256
+            let binary = RGBToBinary(rgbArray[counter++], rgbArray[counter++], rgbArray[counter++]);
+            innerResult = innerResult.shiftLeft(24);
+            innerResult = innerResult.or(binary);
         }
         result.push(innerResult);
     }

f09f98b00be42979aeb4cc101b011d1e578da4c2
Carson Roscoe <CarsonRoscoe7@gmail.com>
2018-01-12 23:32:19 -0800

    Moved SellPixelForm.jsx from divs to tables

--- src/layouts/forms/SellPixelForm.jsx
+++ src/layouts/forms/SellPixelForm.jsx
@@ -19,22 +19,47 @@ class SellPixelForm extends Component {
 
     render() {
         return (
-            <div className='form'>
-                Sell Pixel:
-                <br/>
-                <div>
-                    X: <input id='sellPixelX' type='number' onChange={(e) => this.handleInput('valueX', e)} value={this.state.valueX}></input>
-                </div>
-                <div>
-                    Y: <input id='sellPixelY' type='number' onChange={(e) => this.handleInput('valueY', e)} value={this.state.valueY}></input>
-                </div>
-                <div>
-                    Price: <input id='sellPrice' type='number' onChange={(e) => this.handleInput('valuePrice', e)} value={this.state.valuePrice}></input>
-                </div>
-                <div>
-                    <input type='button' value='Sell Pixel' onClick={() => ctr.sellProperty(this.state.valueX, this.state.valueY, this.state.valuePrice)}></input>
-                </div>
-            </div>
+            <table className='form'>
+                <tbody>
+                    <tr>
+                        <td colSpan={2}>
+                            <div className='title'>
+                                Sell Pixel:
+                            </div>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>
+                            <div className='inputTitle'> X: </div>
+                        </td>
+                        <td>
+                            <input id='sellPixelX' type='number' onChange={(e) => this.handleInput('valueX', e)} value={this.state.valueX}></input>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>
+                            <div className='inputTitle'> Y: </div>
+                        </td>
+                        <td>
+                            <input id='sellPixelY' type='number' onChange={(e) => this.handleInput('valueY', e)} value={this.state.valueY}></input>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>
+                            <div className='inputTitle'> Price: </div>
+                        </td>
+                        <td>
+                            <input id='sellPrice' type='number' onChange={(e) => this.handleInput('valuePrice', e)} value={this.state.valuePrice}></input>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td colSpan={2}>
+                            <input type='button' value='Sell Pixel' onClick={() => ctr.sellProperty(this.state.valueX, this.state.valueY, this.state.valuePrice)}></input>
+                        </td>
+                    </tr>
+                </tbody>
+                
+            </table>
         );
     }
 }

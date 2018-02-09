f68772b1e28077c194d896504cfab38e6c407bcc
Jaegar Sarauer <a00925935>
2018-02-08 17:13:43 -0800

    mini-paint program for designing property pixels

--- src/const/assets.jsx
+++ src/const/assets.jsx
@@ -1,5 +1,13 @@
 
 
-export const ICON_MONEY = '../assets/icons/money.svg';
-export const ICON_LEFT_ARROW = '../assets/icons/left_arrow.svg';
-export const ICON_RIGHT_ARROW = '../assets/icons/right_arrow.svg';
+const BASE_PATH = '../assets/icons/';
+
+export const ICON_MONEY = BASE_PATH + 'money.svg';
+export const ICON_LEFT_ARROW = BASE_PATH + 'left_arrow.svg';
+export const ICON_RIGHT_ARROW = BASE_PATH + 'right_arrow.svg';
+export const BUCKET = BASE_PATH + 'bucket.svg';
+export const DROPPER = BASE_PATH + 'dropper.svg';
+export const ERASE = BASE_PATH + 'erase.svg';
+export const PENCIL = BASE_PATH + 'pencil.svg';
+export const REDO = BASE_PATH + 'redo.svg';
+export const UNDO = BASE_PATH + 'undo.svg';

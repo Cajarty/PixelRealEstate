65ed4d6bcb2ed3edc5de8cea529af256d7da5323
Jaegar Sarauer <a00925935>
2018-04-07 18:30:42 -0700

    mergine updatable contract, JSON TestRPC Resonse Error code:-32603

--- setup-env-dev.sh
+++ setup-env-dev.sh
@@ -1,12 +1,12 @@
 chmod 777 ./destroy-env-dev.sh
 ./destroy-env-dev.sh
-ganache-cli >| ./logs/ganache.txt &
+ganache-cli &> ./logs/ganache.txt &
 echo "kill -9" $! >| destroy-env-dev.sh
-truffle compile
-truffle migrate > ./logs/ctr.txt
+truffle compile &> ./logs/ctr-compile.txt
+truffle migrate --reset --network development &> ./logs/ctr.txt
 cp ./build/contracts/*.json ../PixelRealEstate-Server/build/contracts/
 cd ../PixelRealEstate-Server/
-node . dev cache >| ../PixelRealEstate/logs/server.txt &
+node . dev cache &> ../PixelRealEstate/logs/server.txt &
 cd ../PixelRealEstate/
 echo "kill -9" $! >> destroy-env-dev.sh
-npm run start > ./logs/reactLog.txt
+npm run start

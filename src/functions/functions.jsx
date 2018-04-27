bf6d2cd253302ade4c644d4c93215de05fd647d3
Jaegar Sarauer <a00925935>
2018-04-26 22:57:00 -0700

    Canvas properly loading and sending colors

--- src/functions/functions.jsx
+++ src/functions/functions.jsx
@@ -12,6 +12,7 @@ export const SNUMBER = 16;
 export const LBITS = 3;
 export const LNUMBER = 8;
 export const CONTRACT_DATA_ARRAY_SIZE = 5;
+const COLORS_PER_256 = 20;
 
 export const PAGES = {
     TOP: {query: 'html'},
@@ -184,54 +185,22 @@ export const VisitPage = (path) => {
     browserHistory.push('/' + path);
 }
 
-export const ImageDataToBase64 = ( /*obj[0..999][0..4000]*/ data) => {
-    let result = {};
-    for (let i = 0; i < Object.keys(data).length; i++) {
-        result[i] = [];
-        for (let p = 0; p < result[i].length; p += 4) {
-            let a = data[i][p];
-            for (let char = p + 1; char < p + 3; char++) {
-                a = a << 8;
-                a |= data[i][char];
-            }
-            result[i].push(Base64.btoa(a));
-        }
-        return result;
-    }
-}
-
-export const Base64ToImageData = ( /*obj[0..999][0..500]*/ data) => {   
-    let result = {};
-
-    for (let i = 0; i < Object.keys(data).length; i++) {
-        result[i] = [];
-        for (let p = 0; p < data[i].length; p++) {
-            let tmp = [];
-            let a = parseInt(Base64.atob(data[i][p]));
-            tmp[2] = (a & 255 * 256 * 256) >> 16;
-            tmp[1] = (a & 255 * 256) >> 8;
-            tmp[0] = (a & 255);
-            if (i == 0 && p == 1)
-            result[i].push(tmp[2], tmp[1], tmp[0], 255);
-        }
-    }
-    return result;
-}
-
 export const ContractDataToRGBAArray = (/*uint256[5]*/ contractDataArray) => {
     let result = [];
-    let pixelsPerBigInt = Math.floor(256 / (HBITS + SBITS + LBITS));
      
     for(let i = CONTRACT_DATA_ARRAY_SIZE - 1; i >= 0; i--) {
         let uint256 = bigInt(contractDataArray[i].toString(10), 10);
-        for (let j = 0; j < pixelsPerBigInt; j++) {
-            result.unshift(HNUMBER);
-            result.unshift(uint256.and(HNUMBER).toJSNumber());
-            uint256 = uint256.shiftRight(HBITS); 
-            result.unshift(uint256.and(SNUMBER).toJSNumber());
+        for (let j = 0; j < COLORS_PER_256; j++) {
+            result.unshift(255);
+            result.unshift(uint256.and(LNUMBER - 1).toJSNumber() / LNUMBER);
+            uint256 = uint256.shiftRight(LBITS); 
+            result.unshift(uint256.and(SNUMBER - 1).toJSNumber() / SNUMBER);
             uint256 = uint256.shiftRight(SBITS);
-            result.unshift(uint256.and(LNUMBER).toJSNumber());
-            uint256 = uint256.shiftRight(LBITS);
+            result.unshift(uint256.and(HNUMBER - 1).toJSNumber() / HNUMBER);
+            uint256 = uint256.shiftRight(HBITS);
+            let rgb = HSLtoRGB(result[0], result[1], result[2]);
+            for (let f = 0; f < 3; f++)
+                result[f] = rgb[f];
         }
     }
     return result;
@@ -241,16 +210,16 @@ export const ContractDataToRGBAArray = (/*uint256[5]*/ contractDataArray) => {
 export const RGBArrayToContractData = (rgbArray) => {
     let result = [];
     let counter = 0;
-    let pixelsPerBigInt = Math.floor(256 / (HBITS + SBITS + LBITS));
     for(let i = 0; i < CONTRACT_DATA_ARRAY_SIZE; i++) { //Foreach uint256 in uint256[10]
         let innerResult = new bigInt("0", 10);
-        for(let j = 0; j < pixelsPerBigInt; j++) { //Foreach h, s, l bits for the uint256
-            let binary = RGBToBinary(rgbArray[counter++], rgbArray[counter++], rgbArray[counter++]);
-            counter++;
-            innerResult = innerResult.shiftLeft(pixelsPerBigInt);
+        for(let j = 0; j < COLORS_PER_256; j++) { //Foreach h, s, l bits for the uint256
+            let hsl = RGBtoHSL(rgbArray[counter], rgbArray[counter + 1], rgbArray[counter + 2]);
+            let binary = RGBToBinary(Math.round(Math.min(hsl[0] * HNUMBER, HNUMBER - 1)), Math.round(Math.min(hsl[1] * SNUMBER, SNUMBER - 1)), Math.round(Math.min(hsl[2] * LNUMBER, LNUMBER - 1)));
+            counter+=4;
+            innerResult = innerResult.shiftLeft(HBITS + SBITS + LBITS);
             innerResult = innerResult.or(binary);
         }
-        result.push(new BigNumber(innerResult.toString(), 10));
+        result.push(new BigNumber(innerResult.toString(10), 10));
     }
     return result;
 }
@@ -268,7 +237,7 @@ export const BinaryToRGB = (value) => {
     let obj = { r: 0, g: 0, b: 0 };
     obj.b = LNUMBER;
     obj.g = Math.pow(2, SBITS + LBITS) - obj.b;
-    obj.r = Math.pow(2, HBITS + SBITS + LBITS) - (obj.g + obj.b);
+    obj.r = 1 + Math.pow(2, HBITS + SBITS + LBITS) - (obj.g + obj.b);
 
     obj.g = (obj.g & value) >> HBITS;
     obj.r = (obj.r & value) >> HBITS + SBITS;

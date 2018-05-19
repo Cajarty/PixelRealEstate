220c64fac4bdc0123bd8b468ce211ed6f9988f29
Jaegar Sarauer <a00925935>
2018-05-19 01:42:21 -0700

    Updated whitepaper link

---
+++ src/private/firebaseConfig.jsx
@@ -0,0 +1,17 @@
+export const prodConfig = {
+    apiKey: "AIzaSyBCYj6iXgnCRuEx_y0QxT4Azp7j_--I5wA",
+    authDomain: "pixelpropertyioprod.firebaseapp.com",
+    databaseURL: "https://pixelpropertyioprod.firebaseio.com",
+    projectId: "pixelpropertyioprod",
+    storageBucket: "pixelpropertyioprod.appspot.com",
+    messagingSenderId: "458056041517"
+};
+
+export const devConfig = {    
+    apiKey: "AIzaSyA2-MDQ_StsEiuKX-N8Ju6D_u0seV8q1JA",
+    authDomain: "pixelpropertyiodev.firebaseapp.com",
+    databaseURL: "https://pixelpropertyiodev.firebaseio.com",
+    projectId: "pixelpropertyiodev",
+    storageBucket: "pixelpropertyiodev.appspot.com",
+    messagingSenderId: "647839650864"
+};
